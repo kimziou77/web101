8f52c15d4272e5929b1593baa5d881ee83eb9586
kimziou77 <kimziou77@naver.com>
2022-01-21 18:18:33 +0900

    Feat: Security Filter

---
+++ web101-backend/src/main/java/com/example/web101backend/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.web101backend.config;
+
+import com.example.web101backend.security.filter.JwtAuthenticationFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableWebSecurity
+@Slf4j
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .csrf()
+                    .disable()
+                .httpBasic()
+                    .disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                    .antMatchers("/","/auth/**").permitAll()
+                .anyRequest()
+                    .authenticated();
+
+        http.addFilterAfter(
+                jwtAuthenticationFilter,
+                CorsFilter.class // now : import web.filter
+        );
+    }
+
+}

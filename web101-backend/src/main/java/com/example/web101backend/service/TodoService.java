76467f6b6e54b5aadb08fc0978c26813c19b734a
kimziou77 <kimziou77@naver.com>
2022-01-21 13:11:09 +0900

    Feat: Create Todo Service

--- web101-backend/src/main/java/com/example/web101backend/service/TodoService.java
+++ web101-backend/src/main/java/com/example/web101backend/service/TodoService.java
@@ -2,10 +2,14 @@ package com.example.web101backend.service;
 
 import com.example.web101backend.persistence.TodoEntity;
 import com.example.web101backend.persistence.repository.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@Slf4j
 public class TodoService {
     @Autowired
     private TodoRepository repository;
@@ -14,5 +18,22 @@ public class TodoService {
         repository.save(entity);
         TodoEntity savedEntity = repository.findById(entity.getId()).get();
         return savedEntity.getTitle();
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity){
+        validate(entity);
+        repository.save(entity);
+        log.info("Entity id : {} is saved.",entity.getId());
+        return repository.findByUserId(entity.getUserId());
+    }
+    private void validate(final TodoEntity entity){
+        if(entity == null){
+            log.warn("Entity cannot be null");
+            throw new RuntimeException("Entity cannot be null");
+        }
+        if(entity.getUserId()==null){
+            log.warn("Unknown User.");
+            throw new RuntimeException("Unknown User");
+        }
     }
 }

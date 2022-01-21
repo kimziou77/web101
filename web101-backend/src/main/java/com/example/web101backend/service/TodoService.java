c09cced5ed2aaa89d64ea1d97d3149f07b99564d
kimziou77 <kimziou77@naver.com>
2022-01-21 13:36:27 +0900

    Feat: Update Todo

--- web101-backend/src/main/java/com/example/web101backend/service/TodoService.java
+++ web101-backend/src/main/java/com/example/web101backend/service/TodoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -32,5 +33,15 @@ public class TodoService {
     }
     public List<TodoEntity> retrieve(final String userId){
         return repository.findByUserId(userId);
+    }
+    public List<TodoEntity> update(final TodoEntity entity){
+        validate(entity);
+        final Optional<TodoEntity> original = repository.findById(entity.getId());
+        original.ifPresent(todo->{
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+            repository.save(todo);
+        });
+        return retrieve(entity.getUserId());
     }
 }
